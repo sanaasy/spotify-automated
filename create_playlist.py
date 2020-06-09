362f995b09f11f38180d887703238a6db21089b8
Sanaa Syed <54586561+sanaasy@users.noreply.github.com>
2020-06-09 15:44:09 -0400

    updating methods

--- create_playlist.py
+++ create_playlist.py
@@ -1,12 +1,15 @@
 import json
+from secrets import spotify_user_id, spotify_token
+import requests
 
 
 class CreatePlaylist:
 
     def __init__(self):
-        pass
+        self.user_id = spotify_user_id
 
     # log into youtube
+
     def get_youtube_client(self):
         pass
 
@@ -22,10 +25,37 @@ class CreatePlaylist:
             "public": True
         })
 
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(
+            self.user_id)
+        response = requests.post(
+            query,
+            data=request_body,
+            header={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+        response_json = response.json()
+
+        # playlist id
+        return response_json("id")
+
     # search for the song
-    def get_spotify_url(self):
-        pass
+    def get_spotify_url(self, song_name, artist):
+        query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+            song_name,
+            artist
+        )
+        response = requests.get(
+            query,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(spotify_token)
+            }
+        )
+        response_json = response.json()
 
     # add song into spotify playlist
+
     def add_song_to_playlist(self):
         pass
