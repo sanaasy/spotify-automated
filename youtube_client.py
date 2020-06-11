a5f7ba556dcfb3e4165ac51c0956a63981230ba4
Sanaa Syed <54586561+sanaasy@users.noreply.github.com>
2020-06-11 09:54:42 -0400

    updating python path since imports were shown as errors

--- youtube_client.py
+++ youtube_client.py
@@ -5,14 +5,20 @@ import googleapiclient.discovery
 import youtube_dl
 
 
-class Son(object):
+class Playlist(object):
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
+
+
+class Song(object):
     def __init__(self, artist, track):
         self.artist = artist
         self.track = track
 
 
 class YouTubeClient(object):
-    def __init__(self, crerdentials_location):
+    def __init__(self, credentials_location):
         scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
         # Disable OAuthlib's HTTPS verification when running locally.
@@ -39,7 +45,8 @@ class YouTubeClient(object):
         )
         response = request.execute()
 
-        playlists = [playlist for palylist in response['items']]
+        playlists = [Playlist(item['id'], item['snippet']['title'])
+                     for item in response['items']]
 
         return playlists
 
@@ -53,10 +60,22 @@ class YouTubeClient(object):
 
         response = request.execute()
 
-        for item in ersponse['items']:
+        for item in response['items']:
             video_id = item['snippet']['resourceId']['videoId']
             artist, track = self.get_artist_and_track_from_video(video_id)
             if artist and track:
                 songs.append(Song(artist, track))
 
+        return songs
+
     def get_artist_and_track_from_video(self, video_id):
+        youtube_url = f"https://youtube.com/watch?v={video_id}"
+
+        video = youtube_dl.YoutubeDL({'quiet': True}).extract_info(
+            youtube_url, download=False
+        )
+
+        artist = video['artist']
+        track = video['track']
+
+        return artist, track
